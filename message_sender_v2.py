fdbdd87ad6d28aca3d25c72e261914f25473997e
divy-a <121391127+divy-a@users.noreply.github.com>
2023-07-02 12:28:56 +0530

    Update message_sender_v2.py

--- message_sender_v2.py
+++ message_sender_v2.py
@@ -4,20 +4,19 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-intents = discord.Intents.default()
-client = discord.Client(intents=intents)
+
+def new_client():
+    return discord.Client(intents=discord.Intents.default())
 
 
 def send_message(msg):
-    global MESSAGE
-    MESSAGE = msg
+    client = new_client()
+
+    @client.event
+    async def on_ready():
+        subscriber = await client.fetch_user(int(os.getenv('ID', '')))
+        await subscriber.send(msg)
+
+        await client.close()
+
     client.run(os.getenv('TOKEN', ''))
-
-
-@client.event
-async def on_ready():
-
-    subscriber = await client.fetch_user(int(os.getenv('ID', '')))
-    await subscriber.send(MESSAGE)
-
-    await client.close()
